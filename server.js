449f66e87ec8fea1814d35315cf9f75de58efa6d
umar24nov <umar24nov@gmail.com>
2026-03-05 00:41:04 +0530

    change of key

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.use(express.static('public'));
 // ── Firebase ───────────────────────────────────────────
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId:   process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey:  process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  credential: admin.credential.cert({
+    projectId:   process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey:  process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
     })
   });
 }
